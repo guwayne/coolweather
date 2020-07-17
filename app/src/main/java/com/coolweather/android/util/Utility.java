f083a7e5051aa881ac30e17c3eeae483f9947ca5
guwei1 <guwei1@xiaomi.com>
2020-07-17 18:21:47 +0800

    third commit

--- app/src/main/java/com/coolweather/android/util/Utility.java
+++ app/src/main/java/com/coolweather/android/util/Utility.java
@@ -5,6 +5,9 @@ import android.text.TextUtils;
 import com.coolweather.android.db.City;
 import com.coolweather.android.db.County;
 import com.coolweather.android.db.Province;
+import com.coolweather.android.gson.Weather;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -79,5 +82,19 @@ public class Utility {
         return false;
     }
 
+    /**
+     *将返回的json数据解析成Weather 实体类
+     */
+    public static Weather handleWeatherResponse(String response){
+        try {
+            JSONObject jsonObject = new JSONObject(response);
+            JSONArray jsonArray = jsonObject.getJSONArray("HeWeather");
+            String weatherContent = jsonArray.getJSONObject(0).toString();
+            return new Gson().fromJson(weatherContent,Weather.class);  //将json字符串解析成weather对象
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return null;
+    }
 
 }
